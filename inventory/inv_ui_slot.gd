d8bb6cc462ed262e60d2857af3c1f669f2bed4c8
BradEstacio <bradestacio@gmail.com>
2025-04-06 02:43:17 -0500

    Inventory

--- inventory/inv_ui_slot.gd
+++ inventory/inv_ui_slot.gd
@@ -1,16 +1,39 @@
-extends Panel
+extends Control
+
+@onready var icon = $InnerBorder/ItemIcon
+@onready var item_name = $ItemInfo/NinePatchRect/MarginContainer/ItemTitle/ItemName
+@onready var item_desc = $ItemInfo/NinePatchRect/MarginContainer/ItemTitle/ItemDescription/FlavorText
+
+var item = null
 
 func _ready():
 	pass
 	
 func _process(delta):
 	pass
-
-#@onready var item_visual: Sprite2D = $CenterContainer/Panel/item_display
+	
+#func _on_focus_entered() -> void:
+	#if item != null:
+		#$ItemInfo.set_global_position(Vector2(430, 300))
+		#$ItemInfo.visible = true
 #
-#func update(item: InvItem):
-	#if !item:
-		#item_visual.visible = false
-	#else:
-		#item_visual.visible = true
-		#item_visual.texture = item.texture
+#func _on_focus_exited() -> void:
+	#$ItemInfo.visible = false
+	
+func set_empty():
+	icon.texture = null
+
+func set_item(new_item):
+	item = new_item
+	icon.texture = item["texture"]
+	item_name.text = str(item["name"])
+	item_desc.text = str(item["desc"])
+
+
+func _on_item_button_focus_entered() -> void:
+	if item != null:
+		$ItemInfo.set_global_position(Vector2(430, 300))
+		$ItemInfo.visible = true
+
+func _on_item_button_focus_exited() -> void:
+	$ItemInfo.visible = false
